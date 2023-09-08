7b074cf3042db22c7fe2d066d26b304f846f1441
Венера <125314304+VeneraChepok@users.noreply.github.com>
2023-09-08 20:02:28 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean==bean2);
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+       Cat catBean = (Cat) applicationContext.getBean("cat");
+       Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean==catBean2);
+
     }
 }
